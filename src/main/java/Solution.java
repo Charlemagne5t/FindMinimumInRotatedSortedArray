90672e5fab4c2e624eb8d41b1630bbdaa312a7f5
alek <wait82985@gmail.com>
2023-08-08 18:36:26 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,32 @@
 public class Solution {
     public int findMin(int[] nums) {
         //TODO
-        return 0;
+        int pivot = findPivot(nums);
+
+
+
+        return pivot == nums.length - 1 ? nums[0] : nums[pivot + 1];
+    }
+    public int findPivot(int[] array){
+
+        if(array.length == 0) return  -1;
+
+        int left = 0;
+        int right = array.length - 1;
+        if(array[left] < array[right]) return -1;
+
+
+        while(left < right){
+            int middleIndex = (right + left) / 2;
+            if(array[right] < array[middleIndex]){
+                left = middleIndex;
+            }
+            if(array[left] > array[middleIndex]){
+                right = middleIndex;
+            }
+            if(right - left == 1 && array[left] > array[right]) return left;
+        }
+
+        return -1;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -3,6 +3,15 @@ import org.junit.Test;
 
 public class SolutionTest {
     @Test
+    public void findPivotTest1() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int expected = 3;
+        int actual = new Solution().findPivot(nums);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void findMinTest1() {
         int[] nums = {3, 4, 5, 1, 2};
         int expected = 1;
